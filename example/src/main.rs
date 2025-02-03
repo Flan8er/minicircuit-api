b5bc582f9ed9d8653fdadb56aaaa31a4504247cd
Flan8er <cvaughn@ravenspacesystems.com>
2025-02-03 09:32:53 -0600

    feat: Updated the example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,48 @@
+use minicircuit::{
+    commands::basic::frequency::{
+        GetFrequency, GetFrequencyResponse, SetFrequency, SetFrequencyResponse,
+    },
+    drivers::{
+        communication::write_read, connection::connect_to_signal_generator,
+        properties::TargetProperties,
+    },
+};
+
 fn main() {
-    println!("Hello, world!");
+    let target_properties = TargetProperties::default();
+    let mut port = match connect_to_signal_generator(target_properties) {
+        Some(port) => port,
+        None => {
+            eprintln!("Exiting program: No valid connection.");
+            return;
+        }
+    };
+
+    let command = SetFrequency::default();
+    let command_string: String = command.clone().into();
+    match write_read(&mut *port, command_string) {
+        Ok(response) => {
+            match SetFrequencyResponse::try_from(response) {
+                Ok(_) => println!("Frequency was successfully set to: {}", command.frequency),
+                Err(e) => println!("{}", e),
+            };
+        }
+        Err(e) => {
+            eprintln!("{:#?}\n\t{}", e.kind, e.description)
+        }
+    }
+
+    let command = GetFrequency::default();
+    let command_string: String = command.clone().into();
+    match write_read(&mut *port, command_string) {
+        Ok(response) => {
+            match GetFrequencyResponse::try_from(response) {
+                Ok(response) => println!("Frequency is currently set to: {}", response.frequency),
+                Err(e) => println!("{}", e),
+            };
+        }
+        Err(e) => {
+            eprintln!("{:#?}\n\t{}", e.kind, e.description)
+        }
+    }
 }
